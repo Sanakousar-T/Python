2cf167a14ddda231fa728736c46216c4fca0680f
Sanakousar <60170093+Sanakousar-T@users.noreply.github.com>
2025-10-28 23:01:24 +0530

    Create Q2program.py for string manipulation examples
    Add a Python script to demonstrate string indexing and slicing.

---
+++ python interview/Q2program.py
@@ -0,0 +1,21 @@
+"""
+print the expected results for the below string
+str='india'
+print(str[-1])
+print(str[:-1])
+print(str[::-1])
+"""
+str='india'
+print(str[-1]) #a
+print(str[:-1]) #indi
+print(str[::-1]) #aidni
+
+"""
+var[SI:EI:UP]
+EI- it is excluded
+str[-1] will print element which is at -1 position
+str[:-1] will print all the elements excluding EI(ending index)
+str[::-1] will reverse the string
+"""
+
+
