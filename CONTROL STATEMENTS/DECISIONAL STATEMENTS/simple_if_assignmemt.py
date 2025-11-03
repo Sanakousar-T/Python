1043f52cecffaed637108fe44a0fac4d7bc2cce4
Sanakousar <60170093+Sanakousar-T@users.noreply.github.com>
2025-11-03 12:38:54 +0530

    Create simple_if_assignment.py with assignment questions
    Added assignment questions and a solution for checking if an integer is odd in simple_if_assignment.py.

---
+++ CONTROL STATEMENTS/DECISIONAL STATEMENTS/simple_if_assignmemt.py
@@ -0,0 +1,19 @@
+"""
+ASSIGNMENT QUESTIONS ON simple if : 
+01) WAP to check whether the given integer is odd number. 
+02) WAP to check whether the given character is uppercase. 
+03) WAP to check whether the last value of a list is mutable. 
+04) WAP to consider 2 integers and check whether both are divisible by 
+5 
+05) WAP to check whether the first and last character of string are same. 
+06) WAP to check whether the given data is dictionary 
+07) WAP to check whether the given data is default value.
+"""
+
+#01) WAP to check whether the given integer is odd number.
+num = int(input("enter integer number:"))
+if num%2!=0:
+    print("given integer is odd")
+  # output:
+  #enter integer number:3
+  #given integer is odd
